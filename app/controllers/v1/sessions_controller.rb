b66a1eefd37d49c325557deec979d2cb0cba7a92
Scott Forba <sgforba@gmail.com>
2018-03-20 22:28:49 -0400

    controller scoping

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -1,4 +1,7 @@
 class V1::SessionsController < ApplicationController
+	def show
+		current_user ? head(:ok) : head(:unauthorized)
+	end	
 	def create
 		user = User.where(email: params[:email]).first
 
@@ -11,12 +14,17 @@ class V1::SessionsController < ApplicationController
 
 
 	def destroy
-		current_user.authentication_token = nil
 
-		if current_user.save
+		if nilify_token && current_user.save
 			head(:ok)
 		else
 			head(:unauthorized)
 		end	
 	end
+
+	private
+
+	def nilify_token
+		current_user&.authentication_token = nil
+	end	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319183517) do
+ActiveRecord::Schema.define(version: 20180321021848) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "first_name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20180319183517) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
