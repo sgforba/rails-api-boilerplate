cebc3a72ed1c7208c87b4e1b9f7746e55bed10fd
Scott Forba <sgforba@gmail.com>
2018-03-22 21:53:50 -0400

    create user endpoint

---
+++ app/controllers/v1/users_controller.rb
@@ -0,0 +1,19 @@
+class V1::UsersController < ApplicationController
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			render :create
+		else
+			head(:unprocessable_entity)
+		end	
+	end	
+
+	private
+	
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end	
+
+end
